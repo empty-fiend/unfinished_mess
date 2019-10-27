2bf5c3e6e64cab8809f5979239991a39fcfe4121
Alexey <empty.fiend@icloud.com>
2019-10-27 15:17:33 +0300

    minor changes
    Возвращена навигация мышкой;
    Добавлен новый тип врага, способный стрелять, а так же префаб снаряда;
    Добавлено меню опций.
    + мелкие изменения

---
+++ platformer/Assets/Scripts/enemyScrpts/enemyPatrolScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPatrolScript : MonoBehaviour
+{
+
+    public float speed = 5f, distance = 2f;
+    public bool isFiring = false;
+    public Transform groundDetection;
+    public LayerMask whatIsGround;
+    Collider2D myDeathCollider;
+    public bool movingRight = true;
+    bool amIDeadYet, enemycol;
+    public RaycastHit2D groundInfo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      myDeathCollider = GameObject.FindWithTag("DeathCollider").GetComponent<Collider2D>();
+      isFiring = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      enemycol = Physics2D.IsTouchingLayers(this.GetComponent<Collider2D>(), whatIsGround);
+      groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, whatIsGround);
+      amIDeadYet = Physics2D.IsTouching(myDeathCollider, this.GetComponent<Collider2D>());
+      if (!isFiring && enemycol)
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+      if (groundInfo.collider == false)
+      {
+        if (movingRight == true)
+        {
+          transform.eulerAngles = new Vector3(0, -180, 0);
+          movingRight = false;
+        }
+        else
+        {
+          transform.eulerAngles = new Vector3(0, 0, 0);
+          movingRight = true;
+        }
+      }
+      if (amIDeadYet)
+      {
+        GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().velocity = Vector2.up * 7;
+        Destroy(gameObject);
+      }
+    }
+}
--- platformer/Assets/Scripts/uiScripts/MainMenu.cs
+++ platformer/Assets/Scripts/uiScripts/MainMenu.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
 
     void Start()
     {
-      Cursor.visible = false;
-      Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     public void PlayGame()
