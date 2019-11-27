84dad49e23133d61da120a5aaa872c5a825d6bd8
Alexey <empty.fiend@icloud.com>
2019-11-27 14:29:08 +0300

    Rewrited scripts (mostly)

---
+++ platformer/Assets/Scripts/dealDamageScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class dealDamageScript : MonoBehaviour
+{
+  public void dealtDamage(Collider2D col, int health, int dealDamage)
+  {
+    bool touched = Physics2D.IsTouching(GetComponent<Collider2D>(), col);
+    if (touched)
+    {
+      health -= dealDamage;
+    }
+  }
+}
---
+++ platformer/Assets/Scripts/groundScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class groundScript : MonoBehaviour
+{
+
+  public bool Grounded(Collider2D _collider2D,LayerMask whatIsGround)
+  {
+    return Physics2D.IsTouchingLayers(_collider2D, whatIsGround);
+  }
+}
