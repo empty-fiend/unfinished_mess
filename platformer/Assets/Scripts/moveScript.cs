a16f36911ece0eb3266b0ab1b19365d1d20e5580
empty_fiend <60250455+empty-fiend@users.noreply.github.com>
2020-10-03 09:07:04 +0300

    Revert "Upload project"
    This reverts commit 4282f8e83606dc2e6164db26fc98f5fd809bb315.

---
+++ platformer/Assets/Scripts/moveScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class moveScript : MonoBehaviour
+{
+  public bool _movingRight;
+
+  public void Moving (float speed, bool isShooting)
+  {
+    if (_movingRight)
+    {
+        transform.eulerAngles = new Vector3(0, -180, 0);
+        _movingRight = false;
+    }
+    else if (!_movingRight)
+    {
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        _movingRight = true;
+    }
+    if (!isShooting)
+      transform.Translate(Time.deltaTime * speed * Vector2.right);
+  }
+
+  public void Moving (float move, bool isRunning, float walkSpeed, float runSpeed, Rigidbody2D rb, Transform otherTransform)
+  {
+      if (move < 0 && _movingRight)
+      {
+          otherTransform.eulerAngles = new Vector3(0, -180, 0);
+          _movingRight = false;
+      }
+      else if (move > 0 && !_movingRight)
+      {
+          otherTransform.eulerAngles = new Vector3(0, 0, 0);
+          _movingRight = true;
+      }
+
+      var speed = !isRunning ? walkSpeed : runSpeed;
+
+      rb.velocity = new Vector2(move * speed, rb.velocity.y);
+  }
+}
---
+++ platformer/Assets/Scripts/raycastScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class raycastScript : MonoBehaviour
+{
+  private RaycastHit2D _rayhit;
+
+  public bool IsISeePlayer (Transform playerCheck, LayerMask whoIsYourEnemy, float distance, bool movingRight)
+  {
+    _rayhit = Physics2D.Raycast(playerCheck.position, movingRight ? Vector2.right : Vector2.left, distance, whoIsYourEnemy);
+
+    return _rayhit.collider;
+  }
+
+  public bool IsISeeGround (float distance, Transform groundDetection, LayerMask whatIsGround)
+  {
+    _rayhit = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, whatIsGround);
+
+    return _rayhit.collider;
+  }
+}
